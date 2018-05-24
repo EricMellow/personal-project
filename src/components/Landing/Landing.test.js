c57171e5d6378b9003ca837619a1fadd308b481e
Eric Mellow <ericmellow@gmail.com>
2018-05-24 17:37:03 -0600

    Create form in SignUp component

--- src/components/Landing/Landing.test.js
+++ src/components/Landing/Landing.test.js
@@ -2,7 +2,7 @@ import { Landing } from "./Landing";
 import { shallow } from "enzyme";
 import React from "react";
 
-describe('Navigation', () => {
+describe('Landing', () => {
   let wrapper;
 
   beforeEach(() => {
---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import './SignUp.css';
+
+export class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      error: null
+    };
+  }
+
+  handleInput = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  render() {
+    const {
+      username,
+      email,
+      password,
+      confirmPassword,
+      error
+    } = this.state;
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          value={username}
+          onChange={event => this.handleInput(event)}
+          type="text"
+          placeholder="Full Name"
+          name="username"
+        />
+        <input
+          value={email}
+          onChange={event => this.handleInput(event)}
+          type="text"
+          placeholder="Email Address"
+          name="email"
+        />
+        <input
+          value={password}
+          onChange={event => this.handleInput(event)}
+          type="password"
+          placeholder="Password"
+          name="password"
+        />
+        <input
+          value={confirmPassword}
+          onChange={event => this.handleInput(event)}
+          type="password"
+          placeholder="Confirm Password"
+          name="confirmPassword"
+        />
+        <button type="submit">
+          Sign Up
+        </button>
+
+        {error && <p>Oops!</p>}
+      </form>
+    );
+  }
+}
