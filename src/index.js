f3323ab6d292d716960f72f5ad4884f6986dd798
Eric Mellow <ericmellow@gmail.com>
2018-05-24 14:46:33 -0600

    Add redux and react-redux dependencies

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { rootReducer } from "./reducers/index";
 
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+  && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(rootReducer, devTools);
 
 const application = (
-  <Router>
-    <App />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
 
 ReactDOM.render(application, document.getElementById('root'));
