f900ec88384d34d0cffb9ee67569949e2964d74a
Eric Mellow <ericmellow@gmail.com>
2018-05-26 17:30:11 -0600

    Add userIdReducer tests

---
+++ src/reducers/userId/userIdReducer.test.js
@@ -0,0 +1,39 @@
+import userIdReducer from "./userIdReducer";
+
+describe('userIdReducer', () => {
+
+  it('should return the userId when passed an ADD_USER_ID action', () => {
+    const initialState = null;
+    const mockAction = {
+      type: 'ADD_USER_ID',
+      userId: 'abc123'
+    };
+
+    const result = userIdReducer(initialState, mockAction);
+
+    expect(result).toEqual('abc123');
+  });
+
+  it('should return null when passed an REMOVE_USER_ID action', () => {
+    const initialState = 'abc123';
+    const mockAction = {
+      type: 'REMOVE_USER_ID'
+    };
+
+    const result = userIdReducer(initialState, mockAction);
+
+    expect(result).toEqual(null);
+  });
+
+  it('should return the initialState when passed an action not related to a user id', () => {
+    const initialState = null;
+    const mockAction = {
+      type: 'REMOVE_USER'
+    };
+
+    const result = userIdReducer(initialState, mockAction);
+
+    expect(result).toEqual(initialState);
+  });
+
+});
