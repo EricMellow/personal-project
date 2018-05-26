3aee3dc1441d810cedb770aaf2c8c7fe794b012a
Eric Mellow <ericmellow@gmail.com>
2018-05-26 09:49:42 -0600

    Add handleInput test for SignIn component

--- src/components/SignIn/SignIn.test.js
+++ src/components/SignIn/SignIn.test.js
@@ -12,4 +12,33 @@ describe('SignIn', () => {
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  describe('handleInput', () => {
+
+    it('should set the correct key/value pair in state with the incomming event data', () => {
+      const wrapper = shallow(<SignIn />);
+      const mockEvent = {
+        target: {
+          name: 'password',
+          value: 'trustnoone'
+        }
+      };
+      const initialExpected = {
+        email: '',
+        password: '',
+        error: null
+      };
+      const finalExpected = {
+        email: '',
+        password: 'trustnoone',
+        error: null
+      };
+
+      expect(wrapper.state()).toEqual(initialExpected);
+      wrapper.instance().handleInput(mockEvent);
+      expect(wrapper.state()).toEqual(finalExpected);
+    });
+  });
+
+
 });
