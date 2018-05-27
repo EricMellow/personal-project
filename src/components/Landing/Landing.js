32e2e63274c2bdbd342129e60a9f78a2cd6ae264
Eric Mellow <ericmellow@gmail.com>
2018-05-27 15:52:22 -0600

    Add form and styling to landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -2,11 +2,36 @@ import React, { Component } from "react";
 import './Landing.css'
 
 export class Landing extends Component {
+  constructor(props) {
+    super(props)
+    this.state ={
+      zip: null
+    };
+  }
+
+  handleInputChange = (event) => {
+    event.preventDefault()
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
 
   render() {
     return (
-      <div>
-        Enter your zip code or whatever
+      <div className="landing">
+        <form className="landing-form">
+          <input 
+            name="zip"
+            placeholder="Zip Code"
+            className="zip"
+            type="number"
+            value="this.state.zip"
+            onChange={event => this.handleInputChange(event)}
+          >
+          </input>
+          <h3>Enter your zip code to see activities near you</h3>
+        </form>
       </div>
     );
   }
