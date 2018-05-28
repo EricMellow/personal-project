03394d2de157b2f1035a10243549865333aacbe0
Eric Mellow <ericmellow@gmail.com>
2018-05-28 17:42:35 -0600

    Add marker with mock data

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -7,15 +7,15 @@ class Map extends Component {
     super(props)
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.google !== this.props.google || prevProps.quakes !== this.props.quakes) {
-      this.loadMap();
-    }
-    this.loadMap()
-  }
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevProps.google !== this.props.google || prevProps.quakes !== this.props.quakes) {
+  //     this.loadMap();
+  //   }
+  //   this.loadMap()
+  // }
 
   componentDidMount() {
-    this.loadMap()
+    this.loadMap();
   }
 
   loadMap() {
@@ -29,39 +29,24 @@ class Map extends Component {
         zoom: 12,
         gestureHandling: "cooperative",
         mapTypeId: 'terrain'
-      })
+      });
 
       this.map = new maps.Map(node, mapConfig);
-      // var heatmapData = [];
-      // this.props.quakes.map((quake) => {
-      //   let mag
-      //   if (quake.properties.mag < 5) { mag = 3 } else if (quake.properties.mag > 6) { mag = 10 } else { mag = 5 }
-      //   heatmapData.push({
-      //     location: new google.maps.LatLng(quake.geometry.coordinates[1], quake.geometry.coordinates[0]),
-      //     weight: mag
-      //   })
-      //   const marker = new google.maps.Marker({
-      //     position: { lat: quake.geometry.coordinates[1], lng: quake.geometry.coordinates[0] },
-      //     map: this.map,
-      //     title: quake.properties.title,
-      //     icon: {
-      //       url: //include a URL for specific icon
-      //       }
-      //   });
-      //   var infowindow = new google.maps.InfoWindow({
-      //     content: `<h3>${quake.properties.title}</h3>
-      //     <h4>${(new Date(quake.properties.time)).toDateString()}
-      //     at depth of ${quake.geometry.coordinates[2]} km</h4>`
-      //   });
-      //   marker.addListener('click', function () {
-      //     infowindow.open(this.map, marker);
-      //   });
-      // })
-      // var heatmap = new google.maps.visualization.HeatmapLayer({
-      //   data: heatmapData,
-      //   radius: 35
-      // });
-      // heatmap.setMap(this.map);
+      var mockActivitiesData = [{ lat: 39.730110, lng: -105.069302, duration: '1hr', type: 'basketball'}];
+      mockActivitiesData.map(activity => {
+        const marker = new google.maps.Marker({
+          position: { lat: activity.lat, lng: activity.lng },
+          map: this.map,
+          title: activity.type
+        });
+        var infowindow = new google.maps.InfoWindow({
+          content: `<h3>${activity.type}</h3>
+          <h4>Duration: ${activity.duration}</h4>`
+        });
+        marker.addListener('click', function () {
+          infowindow.open(this.map, marker);
+        });
+      })
     }
   }
   
