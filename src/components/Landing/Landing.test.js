8d16af7cf8431fe24e785b826a9b6b642b98f15d
Eric Mellow <ericmellow@gmail.com>
2018-05-30 10:35:28 -0600

    Add handleInputChange for Landing component

--- src/components/Landing/Landing.test.js
+++ src/components/Landing/Landing.test.js
@@ -12,4 +12,22 @@ describe('Landing', () => {
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  describe('handleInputChange', () => {
+    
+    it('should set state with the event info', () => {
+      const wrapper = shallow(<Landing />);
+      const mockEvent = {
+        target: {
+          name: 'zip',
+          value: 80401,
+        },
+        preventDefault: jest.fn()
+      };
+
+      expect(wrapper.state('zip')).toEqual(undefined)
+      wrapper.instance().handleInputChange(mockEvent)
+      expect(wrapper.state('zip')).toEqual(80401)
+    });
+  });
 });
