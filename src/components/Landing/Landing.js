17c5c44db33145b9060bdd4bd5a5e7e682931379
Eric Mellow <ericmellow@gmail.com>
2018-06-04 15:54:30 -0600

    Change Landing component background and styling

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -3,6 +3,7 @@ import './Landing.css';
 import { connect } from "react-redux";
 import { addZipcode } from "../../actions/zipcodeActions";
 import { getLocation } from "../../apiCalls/apiCalls";
+import { NavLink } from "react-router-dom";
 
 export class Landing extends Component {
   constructor(props) {
@@ -45,8 +46,12 @@ export class Landing extends Component {
             onChange={event => this.handleInputChange(event)}
           >
           </input>
-          <h3>Enter your zip code to see activities near you</h3>
+          <h3 className="enter-text">Enter your zip code to see activities near you</h3>
         </form>
+        <div className="landing-signup">
+          <NavLink to="/signup" className="nav-signup">Sign Up</NavLink>
+          <h3 className="create-text">To create an activity for people to join</h3>
+        </div>
       </div>
     );
   }
