1cc8d811613f626c56ccd6802881c0bdfd28ceea
Eric Mellow <ericmellow@gmail.com>
2018-06-04 11:43:51 -0600

    Add auth tests for firebase

---
+++ src/firebase/auth.test.js
@@ -0,0 +1,47 @@
+import {
+  doCreateUserWithEmailAndPassword,
+  doSignInWithEmailAndPassword,
+  doSignOut
+} from "./auth";
+import { auth } from "./firebase";
+
+jest.mock('firebase', () => ({
+  auth: jest.fn().mockImplementation(() => ({
+    createUserWithEmailAndPassword: jest.fn(),
+    signInWithEmailAndPassword: jest.fn(),
+    signOut: jest.fn()
+  })),
+  apps:[{}],
+  database: jest.fn()
+}));
+
+describe('doCreateUserWithEmailAndPassword', () => {
+
+  it('should call auth.createUserWithEmailAndPassword with the correct arguments', () => {
+    const mockEmail = 'test@test.com';
+    const mockPassword = 'password';
+
+    doCreateUserWithEmailAndPassword(mockEmail, mockPassword);
+    expect(auth.createUserWithEmailAndPassword).toHaveBeenCalledWith(mockEmail, mockPassword);
+  });
+});
+
+describe('doSignInWithEmailAndPassword', () => {
+
+  it('should call auth.signInWithEmailAndPassword with the correct arguments', () => {
+    const mockEmail = 'test@test.com';
+    const mockPassword = 'password';
+
+    doSignInWithEmailAndPassword(mockEmail, mockPassword);
+    expect(auth.signInWithEmailAndPassword).toHaveBeenCalledWith(mockEmail, mockPassword);
+  });
+});
+
+describe('doSignOut', () => {
+
+  it('should call auth.signOut', () => {
+  
+    doSignOut();
+    expect(auth.signOut).toHaveBeenCalled();
+  });
+});
