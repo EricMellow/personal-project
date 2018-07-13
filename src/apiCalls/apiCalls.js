cbdc22c0e17edffb65fbdfba890326c92c123efa
Eric Mellow <ericmellow@gmail.com>
2018-07-13 10:46:17 -0600

    Change key in fetch

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,7 +1,7 @@
-import { firebaseKey } from "../keys";
+// import { firebaseKey } from "../keys";
 
 export const getLocation = async (address) => {
-  const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${firebaseKey}`);
+  const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.FIREBASE_KEY}`);
   const location = await response.json();
   return location.results[0].geometry.location;
 };
